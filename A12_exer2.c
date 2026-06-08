d30eca111c1d6b0aa0414c2bf3dea53018f1b2ab
oliver-oliveira <oliver.oliveira@aluno.ifsp.edu.br>
2026-06-08 08:42:58 -0300

    Add files via upload

--- A12_exer2.c
+++ A12_exer2.c
@@ -8,12 +8,16 @@ int main() {
     scanf("%29s", nome);
 
     for(int i = 0; nome[i] != '\0'; i++) {
+       if(nome[i] >= 'A' && nome[i] <= 'Z') {
+           nome[i] = nome[i] + 32;
+       }
         quant ++;
     }
     printf("Quantidade de letras: %d\n", quant);
     
     printf("Nome invertido: ");
-
+    
+    
     for(int i = quant - 1; i >= 0; i--) {
         printf("%c", nome[i]);
     }
